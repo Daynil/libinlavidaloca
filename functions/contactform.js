d6609cf8658cd97ff61a5995b42d926d920e2dd4
daynil <dlibinrx@gmail.com>
2024-02-25 14:37:00 -0600

    discord contact message

--- functions/contactform.js
+++ functions/contactform.js
@@ -4,37 +4,37 @@ export async function onRequest(context) {
   const email = formData.get("email");
   const message = formData.get("message");
 
-  const data = {
-    personalizations: [
-      {
-        to: [{ email: 'omegasol11@gmail.com' }],
-      },
-    ],
-    from: { email: email },
-    subject: 'libinlavidaloca - Contact Form Message',
-    content: [{ type: 'text/html', value: `Received a contact form message from ${name}, email: ${email}:<br><br>${message}` }],
-  };
+  const alertsRoleTagCode = '<@&858752339761168384>';
+  const DISCORD_MESSAGE_MAX_LENGTH = 2000;
 
   // TODO send email
   try {
-    const res = await fetch("https://api.sendgrid.com/v3/mail/send", {
+    const sentMessage = `Received a contact form message from ${name}, email: ${email}: ${message}`;
+
+    const res = await fetch(context.env.DISCORD_HOOK_URL, {
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${context.env.sendgridkey}`,
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
+      body: {
+        queryParams: { wait: true },
+        content: `${alertsRoleTagCode}, ${sentMessage.substring(
+          0,
+          DISCORD_MESSAGE_MAX_LENGTH - `${alertsRoleTagCode}, ...`.length - 10
+        )}...`
+
+      }
     });
     if (res.ok) {
-      console.log("Successfully sent email: ", name, email, message);
+      console.log("Successfully sent discord msg: ", name, email, message);
       return new Response("Success");
     } else {
-      console.log("Error sending email: ", name, email, message, res.status + " " + res.statusText);
+      console.log("Error sending discord msg: ", name, email, message, res.status + " " + res.statusText);
       console.log('res', await res.json())
       return new Response("Error", { status: 500 });
     }
   } catch (e) {
-    console.log("Error sending email: ", name, email, message, e);
+    console.log("Error sending discord msg: ", name, email, message, e);
     return new Response("Error", { status: 500 });
   }
 }
